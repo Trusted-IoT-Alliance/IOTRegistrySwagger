8645991c7bb278ffe2b722d83963abb5a7e34271
Robert Nowell <robert.j.nowell@gmail.com>
2017-03-08 12:20:44 -0800

    add images

--- examples/config.js
+++ examples/config.js
@@ -9,15 +9,15 @@ var config = {
     },
 
     creds: {
-        name: 'test_user4',
-        password: '4nXSrfoYGFCP'
+        name: 'test_user6',
+        password: 'b7pmSxzKNFiw'
     },
     privKey: new bitcore.PrivateKey('d97b34bf2ada55ea1f7c8a6d81e92a1ab0b9b160a1f4f4b711d4738b128c6c8a'),
 
     members: process.env.TUXEDOPOPSWARE_MEMBERS || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7054',
     peer: process.env.TUXEDOPOPSWARE_PEER || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7051',
     events: process.env.TUXEDOPOPSWARE_EVENTS || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7053',
-    chaincodeName: process.env.TUXEDOPOPSWARE_CHAINCODE_NAME || "92b992781c0b6756ada79c8ff46acca232111026b4351d3f6c84c201606e0a6ae6c5af5115f042af2e24ee6e1ae42076d1cbbbea509d4c93f689f79e3be8f630",
+    chaincodeName: process.env.TUXEDOPOPSWARE_CHAINCODE_NAME || "a4e05bc298633458cc6dc0fae58a05f370f6b1136fccdb29bb837ff53c661f20dc2fabbc08353a1c133164ef445f8b759048996dea322468a7c6e72ee1f21af4",
 }
 console.log(config);
 module.exports = config;
