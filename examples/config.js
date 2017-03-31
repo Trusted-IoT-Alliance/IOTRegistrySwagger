dd6a2f76b0dc5e92c59d227b83ea109fc1624b9f
Robert Nowell <robert.j.nowell@gmail.com>
2017-03-31 11:11:37 -0700

    rename environment variables from config.js

--- examples/config.js
+++ examples/config.js
@@ -2,22 +2,22 @@ var fs = require('fs');
 var bitcore = require('bitcore-lib');
 var config = {
     host: process.env.FQDN || 'localhost',
-    port: process.env.TUXEDOPOPSWARE_PORT || 8082,
+    port: process.env.IOTREGISTRY_PORT || 8082,
 
     store: {
-        path: process.env.TUXEDOPOPSWARE_STORE_PATH || 'iotregistryware'
+        path: process.env.IOTREGISTRY_STORE_PATH || 'iotregistryware'
     },
 
     creds: {
-        name: 'test_user6',
-        password: 'b7pmSxzKNFiw'
+        name: 'test_user4',
+        password: '4nXSrfoYGFCP'
     },
     privKey: new bitcore.PrivateKey('d97b34bf2ada55ea1f7c8a6d81e92a1ab0b9b160a1f4f4b711d4738b128c6c8a'),
 
-    members: process.env.TUXEDOPOPSWARE_MEMBERS || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7054',
-    peer: process.env.TUXEDOPOPSWARE_PEER || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7051',
-    events: process.env.TUXEDOPOPSWARE_EVENTS || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7053',
-    chaincodeName: process.env.TUXEDOPOPSWARE_CHAINCODE_NAME || "a4e05bc298633458cc6dc0fae58a05f370f6b1136fccdb29bb837ff53c661f20dc2fabbc08353a1c133164ef445f8b759048996dea322468a7c6e72ee1f21af4",
+    members: process.env.IOTREGISTRY_MEMBERS || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7054',
+    peer: process.env.IOTREGISTRY_PEER || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7051',
+    events: process.env.IOTREGISTRY_EVENTS || 'grpc://ec2-54-219-181-242.us-west-1.compute.amazonaws.com:7053',
+    chaincodeName: process.env.IOTREGISTRY_CHAINCODE_NAME || "64d314667b65b9249fc070067bc6c54facfa14c831d79f63b1940b1fd50b5d70c33e1890c021bb65dd3a393ad192364b8ef08c366343130582ce2ea84044f3e5",
 }
 console.log(config);
 module.exports = config;
